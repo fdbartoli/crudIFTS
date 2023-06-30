6281dd41313aabfd8f978d6fe30b816a60cacb36
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2023-06-30 20:39:22 -0300

    component show-item

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { ShowItemComponent } from './components/show-item/show-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservaComponent,
+    ShowItemComponent
   ],
   imports: [
     BrowserModule,
