c20ea9b3fa5379114455e25d2c652e060560dd4e
Claudia Fernandez <claudiafernandezdev@gmail.com>
2023-07-01 01:14:04 -0300

    se crea servicio para editar

---
+++ frontEnd/src/app/components/editar-agregar-item/editar-agregar-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-agregar-item',
+  templateUrl: './editar-agregar-item.component.html',
+  styleUrls: ['./editar-agregar-item.component.css'],
+})
+export class EditarAgregarItemComponent {}
